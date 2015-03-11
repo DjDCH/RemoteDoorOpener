6ef4bd8bee4faa9e69d95d3c12b706343cf51492
DjDCH <coding@djdch.com>
2015-03-10 21:44:13 -0400

    Added missing serialVersionUID.

--- AMQP/src/main/java/com/djdch/dev/rdo/amqp/exception/PassiveDeclareException.java
+++ AMQP/src/main/java/com/djdch/dev/rdo/amqp/exception/PassiveDeclareException.java
@@ -2,6 +2,8 @@ package com.djdch.dev.rdo.amqp.exception;
 
 public class PassiveDeclareException extends RuntimeException {
 
+    private static final long serialVersionUID = -2921599750107191701L;
+
     public PassiveDeclareException(Exception e) {
         super(e);
     }
